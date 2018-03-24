bc71c4066f163bd1400387bdfe473cb5d28c23dc
Chady Aboulhosn <aboulhosnc@vcu.edu>
2018-03-24 03:18:39 -0400

    Add signup page and xml Add login page and xml Add to manifest file No buttons work

---
+++ app/src/main/java/com/example/chady/artifacts/Login_Page.java
@@ -0,0 +1,16 @@
+package com.example.chady.artifacts;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Chady on 3/24/2018.
+ */
+
+public class Login_Page extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_page);
+    }
+}
