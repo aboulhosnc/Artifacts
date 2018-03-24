9a871423c6ea82d636121b635552f2b659a41334
mikeP-1107 <mpoblacion92@gmail.com>
2018-03-24 12:50:29 -0400

    Updated Button Functionalities for login and signup.. Also put in HomeScreen with ONLY artifactsPage button currently functional.

--- app/src/main/java/com/example/chady/artifacts/Login_Page.java
+++ app/src/main/java/com/example/chady/artifacts/Login_Page.java
@@ -1,10 +1,14 @@
 package com.example.chady.artifacts;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 /**
  * Created by Chady on 3/24/2018.
+ * Edited by Michael on 3/24/2018.
+ *
  */
 
 public class Login_Page extends Activity {
@@ -13,4 +17,12 @@ public class Login_Page extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_page);
     }
+    public void onUserLoginClick(View view){
+
+        //IF-ELSE STATEMENT TO BE IMPLEMENTED USER IS SIGNED UP AND MATCHES DATABASE validateLogin()
+        Intent i = new Intent(Login_Page.this, HomeScreen.class);
+        startActivity(i);
+
+    }
+    //public boolean validateLogin();
 }
--- app/src/main/java/com/example/chady/artifacts/MainActivity.java
+++ app/src/main/java/com/example/chady/artifacts/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.chady.artifacts;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    ///When the user clicks Login ... Brings user to the login screen
+    public void onLoginClick(View view){
+        Intent i = new Intent(MainActivity.this, Login_Page.class);
+        startActivity(i);
+    }
+    //When the user clicks Sign Up ... Brings User to the sign up screen
+    public void onSignUpClick(View view){
+        Intent i = new Intent(MainActivity.this, Signup.class);
+        startActivity(i);
     }
 }
